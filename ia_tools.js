1adcfdae735b41e7d665ff55e5e118cf6e51c453
ialej001 <ivan.alejandre@gmail.com>
2019-08-15 18:42:15 -0700

    Update ia_tools.js

--- ia_tools.js
+++ ia_tools.js
@@ -7,6 +7,37 @@ const mysql = require('mysql');
 
 module.exports = {
 
+    Database: class {
+        constructor() {
+            this.connection = mysql.createPool({
+                host: "us-cdbr-iron-east-02.cleardb.net",
+                user: "b91f42fc83bc56",
+                password: "70d8e8dd",
+                database:"heroku_32cdeda847586a8"
+            })
+        }
+
+        query(sql, args) {
+            return new Promise((resolve, reject) => {
+                this.connection.query(sql, args, (err, rows) => {
+                    if (err)
+                        return reject(err);
+                    resolve(rows);
+                });
+            });
+        }
+
+        close() {
+            return new Promise((resolve, reject) => {
+                this.connection.release(err => {
+                    if (err)
+                        return reject(err);
+                    resolve();
+                });
+            });
+        }
+    },
+    
     createSqlDb_connection: function() {
         var conn = mysql.createConnection({
             host: "us-cdbr-iron-east-02.cleardb.net",
