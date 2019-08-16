e5df17edd8e3c837b289a8800b352c2b93671bc2
ialej001 <ivan.alejandre@gmail.com>
2019-08-15 19:10:16 -0700

    Update ia_tools.js

--- ia_tools.js
+++ ia_tools.js
@@ -37,16 +37,17 @@ module.exports = {
             });
         }
     },
-    
-    createSqlDb_connection: function() {
+
+    createSqlDb_connection: function () {
         var conn = mysql.createConnection({
             host: "us-cdbr-iron-east-02.cleardb.net",
             user: "b91f42fc83bc56",
             password: "70d8e8dd",
             database: "heroku_32cdeda847586a8"
-        });
+        })
         return conn;
     },
+
 
     sendQuery: function (sql, param, conn) {
         return new Promise(function (resolve, reject) {
@@ -54,10 +55,8 @@ module.exports = {
                 //if (err) throw err;
                 if (!results || err) {
                     reject( err );
-                    conn.end;
                 } else {
                     resolve ( results );
-                    conn.end;
                 }
             });
         });
@@ -68,7 +67,6 @@ module.exports = {
             conn.query(sql, param, function (err) {
                 if (err) throw err;
                 resolve();
-                conn.end;
             });
         });
     }
